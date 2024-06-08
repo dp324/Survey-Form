9bb3a4f9bdd3fedd201b868ae084866c5ec21b1c
Deepanshu <deepanshusharma324@gmail.com>
2024-06-09 00:06:27 +0530

    cors3

--- backend/index.js
+++ backend/index.js
@@ -11,22 +11,12 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 
-const corsConfig = {
-    origin : "http://localhost:5173",
-    credentials: true,
-    origin: true,
-};
-app.use(cors(corsConfig));
-
-//app.use(cors());
-// app.use(
-//     cors({
-//         origin : ['http://localhost:5173'],
-//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//         allowedHeaders: ['content'],
-//         credentials : [true]
-//     })
-// )
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}))
 
 
 app.use(cookieParser());
